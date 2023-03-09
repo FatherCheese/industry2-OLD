1560c1100ef70e66e5acb15f8fdf9b5500cbcdb5
FatherCheese <ythojim@gmail.com>
2023-03-09 09:59:50 -0700

    Basic Blocks
    +Added basic ore and material blocks

--- src/main/java/turniplabs/industry/ModIndustry2.java
+++ src/main/java/turniplabs/industry/ModIndustry2.java
@@ -32,6 +32,8 @@ public class ModIndustry2 implements ModInitializer {
     public static final Block ORE_COPPER_BASALT = BlockHelper.createBlock(MOD_ID, new BlockOreCopper(nextBlock(), Material.rock), "ore.copper.basalt", "ore_copper_basalt.png", Block.soundStoneFootstep, 3.0f, 3.0f, 0.0f);
     public static final Block ORE_COPPER_LIMESTONE = BlockHelper.createBlock(MOD_ID, new BlockOreCopper(nextBlock(), Material.rock), "ore.copper.limestone", "ore_copper_limestone.png", Block.soundStoneFootstep, 3.0f, 3.0f, 0.0f);
     public static final Block ORE_COPPER_GRANITE = BlockHelper.createBlock(MOD_ID, new BlockOreCopper(nextBlock(), Material.rock), "ore.copper.granite", "ore_copper_granite.png", Block.soundStoneFootstep, 3.0f, 3.0f, 0.0f);
+    public static final Block BLOCK_OF_COPPER = BlockHelper.createBlock(MOD_ID, new Block(nextBlock(), Material.iron), "block.copper", "block_copper_top.png", "block_copper_bottom.png", "block_copper_sides.png", Block.soundMetalFootstep, 5.0f, 6.0f, 0.0f);
+
     public static final Item RAW_COPPER = ItemHelper.createItem(MOD_ID, new Item(nextItem()), "ore.raw.copper", "raw_copper.png");
     public static final Item DUST_COPPER = ItemHelper.createItem(MOD_ID, new Item(nextItem()), "dust.copper", "dust_copper.png");
     public static final Item INGOT_COPPER = ItemHelper.createItem(MOD_ID, new Item(nextItem()), "ingot.copper", "ingot_copper.png");
@@ -41,11 +43,15 @@ public class ModIndustry2 implements ModInitializer {
     public static final Block ORE_TIN_BASALT = BlockHelper.createBlock(MOD_ID, new BlockOreTin(nextBlock(), Material.rock), "ore.tin.basalt", "ore_tin_basalt.png", Block.soundStoneFootstep, 3.0f, 3.0f, 0.0f);
     public static final Block ORE_TIN_LIMESTONE = BlockHelper.createBlock(MOD_ID, new BlockOreTin(nextBlock(), Material.rock), "ore.tin.limestone", "ore_tin_limestone.png", Block.soundStoneFootstep, 3.0f, 3.0f, 0.0f);
     public static final Block ORE_TIN_GRANITE = BlockHelper.createBlock(MOD_ID, new BlockOreTin(nextBlock(), Material.rock), "ore.tin.granite", "ore_tin_granite.png", Block.soundStoneFootstep, 3.0f, 3.0f, 0.0f);
+    public static final Block BLOCK_OF_TIN = BlockHelper.createBlock(MOD_ID, new Block(nextBlock(), Material.iron), "block.tin", "block_tin_top.png", "block_tin_bottom.png", "block_tin_sides.png", Block.soundMetalFootstep, 5.0f, 6.0f, 0.0f);
+
     public static final Item RAW_TIN = ItemHelper.createItem(MOD_ID, new Item(nextItem()), "ore.raw.tin", "raw_tin.png");
     public static final Item DUST_TIN = ItemHelper.createItem(MOD_ID, new Item(nextItem()), "dust.tin", "dust_tin.png");
     public static final Item INGOT_TIN = ItemHelper.createItem(MOD_ID, new Item(nextItem()), "ingot.tin", "ingot_tin.png");
 
     // Misc //
+    public static final Block BLOCK_OF_BRONZE = BlockHelper.createBlock(MOD_ID, new Block(nextBlock(), Material.iron), "block.bronze", "block_bronze_top.png", "block_bronze_bottom.png", "block_bronze_sides.png", Block.soundMetalFootstep, 5.0f, 6.0f, 0.0f);
+
     public static final Item DUST_BRONZE = ItemHelper.createItem(MOD_ID, new Item(nextItem()), "dust.bronze", "dust_bronze.png");
     public static final Item INGOT_BRONZE = ItemHelper.createItem(MOD_ID, new Item(nextItem()), "ingot.bronze", "ingot_bronze.png");
 
